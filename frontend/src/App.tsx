c2a4e5f1976961ef79c95c9ca1e75b69377338bb
Francisco Javier Català Balta <fcatala-@cbr9s6.42barcelona.com>
2025-12-20 08:47:53 +0100

    Image of Blas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Blas is comming soon!</h1>
+      <div className="card">
+      <img src="/lezo.jpg" alt="Blas Logo" style={{ width: '200px' }} />
+      </div>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
